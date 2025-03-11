630eff2e9e44bde6d4cd3693532c3fb56fcc83c4
VivekCitrusbug <vivek1.citrusbug@gmail.com>
2025-03-11 12:27:18 +0530

    minor changes and format

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ import tempfile
 import os
 import subprocess
 
+def process_image(image_path, model):
+    image = cv2.imread(image_path)
+    results = model(image)
+    
+    for result in results:
+        for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = box.conf[0].item()
+            cls = int(box.cls[0].item())
+            label = f"{model.names[cls]}: {conf:.2f}"
+            
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+    return image
+
 def process_video(video_path, model):
     cap = cv2.VideoCapture(video_path)
     fourcc = cv2.VideoWriter_fourcc(*'MP4V')
@@ -47,7 +63,7 @@ st.title("Object Detection using YOLOv8")
 uploaded_file = st.file_uploader("Upload an image or video", type=["jpg", "jpeg", "png", "mp4", "avi", "mov"])
 
 if uploaded_file is not None:
-    model = YOLO("C:/Users/vivek/VivekInternship/Temporary/Models/Model2_without_aug.pt")
+    model = YOLO("C:/Users/vivek/VivekInternship/AI_powered_construction_site_ppe_detection/models/Yolov8n_without_aug.pt")
     
     with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[-1]) as temp_file:
         temp_file.write(uploaded_file.read())
@@ -60,3 +76,6 @@ if uploaded_file is not None:
         print("/n Output_Video_path : ",output_video_path)
         st.write("### Processed Video 📹")
         st.video(output_video_path) 
+    elif uploaded_file.type.startswith("image"):
+        processed_image = process_image(temp_path, model)
+        st.image(processed_image, caption="Processed Image", channels="BGR")
